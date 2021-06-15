a22a954f01e4ddcc524d3cc4c705275ba2881313
Ijustusethistomakeneverendinglegacymods <59239511+Ijustusethistomakeneverendinglegacymods@users.noreply.github.com>
2021-06-15 18:04:43 -0400

    Update Tent_mod.js

--- Tent_mod.js
+++ Tent_mod.js
@@ -1,36 +1,41 @@
 G.AddData({
-    name:'Tents Mod',
-    author:'Wood Chair',
-    desc:'This is just a solution to the endgame problem of not having engough land for housing. Don’t ask about graves still taking up land and things, i just forgot to add a solution to them.',
-    engineVersion:1,
-    manifest:0,
-    requires:['Default dataset*'],
-    sheets:{'spicySheet':'img/spicyModIconSheet.png'}
-    func:function()
-    {
-		new G.Res({
-		name:'Tents',
-		desc:'Tents provide an easy solution to the late game problem of not having any land. this allows you to continue playing without hitting the wall of land.',
-		icon:[0,0,'spicySheet'],
-		partOf:'misc materials',
-		category:'misc',
-    });
+name:'Tent mod',
+author:'Wood Chair',
+desc:'Tents.',
+engineVersion:1,
+manifest:0,
+requires:['Default dataset*'],
+sheets:{'spicySheet':'img/spicyModIconSheet.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function()
+{
 		new G.Unit({
-		name: 'Tent Maker',
-		desc: 'Creates [Tents] out of leather.',
+		name:'tent',
+		desc:'@People can sleep here',
 		icon:[0,0,'spicySheet'],
-		cost:{},
-		req:{'Tent Making':true},
-		use:{'Worker:1},
-		staff:{'stone tools':1},
-		upkeep:{}
+		cost:{'food':50},
 		effects:[
-		    {type:'convert',from:{'Leather':5},into:{'Tents':1},chance:10/10,every:1},
-    });
-		new G.Tech({
-		name:'Tent Making',
-		desc:'Unlocks the making of [Tents].'
+			{type:'provide',what:{'housing':1}}
+		],
+		category:'housing',
+		req:{'tent making':true},
+	});
+	new G.Tech({
+		name:'tent making',
+		desc:'@You can now make tents.',
 		icon:[0,0,'spicySheet'],
-		cost:{'Insight':80},
+		cost:{'insight':15},
 		req:{'sedentism':true},
-    });
+	});
+	//Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Hot sauce preparing" tech) is fulfilled.
+	//new G.Trait({
+		//name:'tent sharing',
+		//desc:'@your people will now share tents with one another, allowing tents to provide more housing.',
+		//icon:[0,0,'spicySheet'],
+		//chance:3,
+		//req:{'tent making':true},
+		//effects:[
+			//{type:'function',func:function(){G.getDict('hot sauce').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
+		//],
+	//});
+}
+});
