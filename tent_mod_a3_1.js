e58773c577faf20dbb9658cfd9bc60c6b5ae31f9
Wood-Chair <59239511+Wood-Chair@users.noreply.github.com>
2021-06-17 12:15:47 -0400

    Update and rename tent_mod_a2_8.js to tent_mod_a3_1.js

--- tent_mod_a3_1.js
+++ tent_mod_a3_1.js
@@ -1,7 +1,7 @@
 G.AddData({
 name:'Tents +more! mod',
 author:'Wood Chair',
-desc:'A mod that adds tents +more!.',
+desc:'A mod that adds tents +more!. Disclaimer:"More!" may include cannabalism. Be warned.',
 engineVersion:1,
 manifest:0,
 requires:['Default dataset*'],
@@ -36,7 +36,6 @@ func:function()
 		effects:[
 			{type:'function',func:function(){G.getDict('herb').turnToByContext['eating']['happiness']=0.03;}},
 		],
-
 	});
   		new G.Unit({
 		name:'murderer',
@@ -46,9 +45,11 @@ func:function()
 		staff:{'stone weapons':1},
 		modes:{
 			'kill':{name:'Commit murder',icon:[5,4],desc:'Just good old fashioned murder.'},
+			'cannibal':{name:'Cannabalize',icon:[6,9],desc:'Convert.',req:{'cannabalism':true}},
 		},
 		effects:[
 			{type:'convert',from:{'adult':1},into:{'corpse':1},every:30,mode:'kill'},
+			{type:'convert',from:{'corpse':1},into:{'meat':30,'bone':5},every:1,mode:'cannibal'},
 		],
 		category:'housing',
 		req:{'murder':true},
@@ -60,6 +61,20 @@ func:function()
 		cost:{'insight':5},
 		req:{'spears':true},
 	});
+		new G.Tech({
+		name:'cannabalism',
+		desc:'@Allows [murderer, Murderers] to convert corpses into [meat] and [bone].<>Wait, the [murderer, Murderers] are actually useful?',
+		icon:[27,7],
+		cost:{'insight':10},
+		req:{'murder':true,'cooking':true},
+	});
+	//	new G.Tech({
+	//	name:'cremation',
+	//	desc:'@Allows [firekeeper]s to burn corpses.<>This doesnt create any useful biproducts, but removes bodies at a faster rate.',
+	//	icon:[27,7],
+	//	cost:{'insight':10},
+	//	req:{'murder':true,'cooking':true},
+	//});
   
 }
 });
