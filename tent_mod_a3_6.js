f86750f5875deda311b2bf88afd382588c0c540c
Wood-Chair <59239511+Wood-Chair@users.noreply.github.com>
2021-06-20 13:55:46 -0400

    Update and rename tent_mod_a3_5.js to tent_mod_a3_6.js

--- tent_mod_a3_6.js
+++ tent_mod_a3_6.js
@@ -28,7 +28,7 @@ func:function()
 	});
 	
 	new G.Trait({
-		name:'herb-eating',
+		name:'herb eating',
 		desc:'@Your people are no longer unhappy when eating [herb]s',
 		icon:[4,6,22,1],
 		chance:10,
@@ -37,6 +37,19 @@ func:function()
 			{type:'function',func:function(){G.getDict('herb').turnToByContext['eating']['happiness']=0.03;}},
 		],
 	});
+	new G.Trait({
+		name:'cooked foods',
+		desc:'@Your people are used to eating [cooked meat, cooked] foods. They gain more happiness from [cooked meat, cooked] foods, but lose more from [meat, raw] meats.',
+		icon:[6,7,22,1],
+		cost:{'cooked meat':20},
+		chance:10,
+		req:{'cooking':true},
+		effects:[
+			{type:'function',func:function(){G.getDict('cooked meat').turnToByContext['eating']['happiness']=0.06;}},
+			{type:'function',func:function(){G.getDict('meat').turnToByContext['eating']['happiness']=-0.01;}},
+		],
+	});
+	
   		new G.Unit({
 		name:'murderer',
 		desc:'@Kills people<>Dont hire these! All they do is murder!.',
@@ -45,7 +58,7 @@ func:function()
 		staff:{'stone weapons':1},
 		gizmos:true,
 		modes:{
-			'kill':{name:'Commit murder',icon:[5,4],desc:'Just good old fashioned murder.'},
+			'kill':{name:'Commit murder',icon:[5,4],desc:'Just good old fashioned murder. Not good for [happiness, morale] though!'},
 			'cannibal':{name:'Cannabalize',icon:[6,9],desc:'Turn [corpse]s into meat! Finally, a use for those things...',req:{'cannabalism':true}},
 		},
 		effects:[
@@ -69,5 +82,45 @@ func:function()
 		cost:{'insight':10},
 		req:{'murder':true,'cooking':true},
 	});
+	
+	//new G.Unit({
+		//name:'barn',
+		//desc:'@provides 4000 [food storage]<>A large wooden building for storing food. A worker manages the grain to prevent rot.',
+		//icon:[24,4],
+		//cost:{'basic building materials':500},
+		//use:{'land':2},
+		//staff:{'worker':1},
+		//require:{'worker':2,'stone tools':2},
+		//effects:[
+		//	{type:'provide',what:{'added food storage':4000}},
+		//	{type:'waste',chance:0.001/1000}
+		//],
+		//req:{'stockpiling':true,'carpentry':true},
+		//category:'storage',
+	//});
+	//new G.Tech({
+		//name:'medical science',
+		//desc:'@Unlocks [hospital] unit<>So, you have [stone weapons, Weapons] now, why not use those on people?',
+		//icon:[18,1],
+		//cost:{'insight':5},
+		//req:{'spears':true},
+	//});
+	//new G.Unit({
+		//name:'hospital',
+		//desc:'@Heals the [sick] and the [wounded] faster than a [healer] using more complex medicine.<>Hospitals gather workers together to heal and research.',
+		//icon:[23,3],
+		//cost:{'basic building materials':500},
+		//use:{'worker':3},
+		//staff:{'stone tools':3},
+		//upkeep:{'coin':0.2},
+		//effects:[
+			//{type:'convert',from:{'sick':1,'herb':2.5},into:{'adult':1},chance:4/5,every:3},
+			//{type:'convert',from:{'wounded':1,'herb':2.5},into:{'adult':1},chance:3/5,every:10},
+		//],
+		//req:{'medical science':true},
+		//category:'civil',
+		//priority:5,
+	//});
+	
 }
 });
