9e445f3a1723857527855b6db7eee258b83725f4
Wood-Chair <59239511+Wood-Chair@users.noreply.github.com>
2025-07-28 01:20:47 -0400

    Update ten_mod.js

--- ten_mod.js
+++ ten_mod.js
@@ -26,16 +26,5 @@ func:function()
 		cost:{'insight':15},
 		req:{'sedentism':true},
 	});
-	//Finally, we add a trait that amplifies the benefits of consuming hot sauce; it will take on average 20 years to appear once the conditions (knowing the "Hot sauce preparing" tech) is fulfilled.
-	//new G.Trait({
-		//name:'tent sharing',
-		//desc:'@your people will now share tents with one another, allowing tents to provide more housing.',
-		//icon:[0,0,'spicySheet'],
-		//chance:3,
-		//req:{'tent making':true},
-		//effects:[
-			//{type:'function',func:function(){G.getDict('hot sauce').turnToByContext['eat']['happiness']=0.2;}},//this is a custom function executed when we gain the trait
-		//],
-	//});
 }
 });
